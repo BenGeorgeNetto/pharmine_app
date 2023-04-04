e9159ab12b3b6bc12d39d9b84d6e2a40a8e13f85
Ben George Netto <bengnetto245@gmail.com>
2023-04-05 01:13:34 +0530

    add profile

--- lib/presentaton/screens/profile_screen.dart
+++ lib/presentaton/screens/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pharmine_app/theme/colors.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -9,8 +10,74 @@ class ProfileScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Profile'),
       ),
-      body: const Center(
-        child: Text('Profile'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 32.0),
+                  const CircleAvatar(
+                    radius: 64,
+                    backgroundImage: AssetImage('lib/assets/images/profile_pic.png'),
+                  ),
+                  const SizedBox(height: 16.0),
+                  Text(
+                    'Rick Astley',
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  Text(
+                    'rickastley@gmail.com',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                  Text(
+                    'Age: 69 Years',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Address',
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                      Icon(
+                        Icons.edit_outlined,
+                        color: darkGrey,
+                      )
+                    ],
+                  ),
+                  // TODO: Make address editable using button
+                  Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text(
+                            '1234, Main Street, New York, USA',
+                            style: Theme.of(context).textTheme.bodyLarge,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
