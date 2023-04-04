65d98473ff3cc7840246b7fae5e35990aa40142b
Ben George Netto <bengnetto245@gmail.com>
2023-04-04 22:14:09 +0530

    add settings page

--- lib/presentaton/screens/settings_screen.dart
+++ lib/presentaton/screens/settings_screen.dart
@@ -7,14 +7,92 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(
+    return Scaffold(
+      appBar: const CustomAppBar(
         title: 'Settings',
       ),
-      body: Center(
-        child: Text('Settings'),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                SettingsElementHeading(heading: 'General'),
+                SettingsElement(option: 'Emergency Contact'),
+                Divider(
+                  thickness: 2.0,
+                ),
+                SettingsElement(option: 'Theme'),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                SettingsElementHeading(heading: 'Payment Settings'),
+                SettingsElement(option: 'Payment Options'),
+                Divider(
+                  thickness: 2.0,
+                ),
+                SettingsElement(option: 'Subscription'),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                SettingsElementHeading(heading: 'Notification Settings'),
+                SettingsElement(option: 'Alarm'),
+                Divider(
+                  thickness: 2.0,
+                ),
+                SettingsElement(option: 'Notifications'),
+              ],
+            ),
+          ),
+        ],
       ),
-      bottomNavigationBar: CustomBottomNavBar(),
+      bottomNavigationBar: const CustomBottomNavBar(),
+    );
+  }
+}
+
+class SettingsElement extends StatelessWidget {
+  final String option;
+  const SettingsElement({super.key, required this.option});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(
+        option,
+        style: Theme.of(context).textTheme.bodyLarge,
+      ),
+    );
+  }
+}
+
+class SettingsElementHeading extends StatelessWidget {
+  final String heading;
+  const SettingsElementHeading({super.key, required this.heading});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(
+        heading,
+        style: Theme.of(context).textTheme.titleMedium,
+      ),
     );
   }
 }
