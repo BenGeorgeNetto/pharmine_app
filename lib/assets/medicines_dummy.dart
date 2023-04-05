5cb6e6f23640d140a319c3b7cff53d08e0f0eb6c
Aditya <me@xditya.me>
2023-04-05 07:25:18 +0530

    chore: fix medicine image links

--- lib/assets/medicines_dummy.dart
+++ lib/assets/medicines_dummy.dart
@@ -2,7 +2,7 @@ final medicines_dummy_list = {
   "Aspirin": {
     "medicine_id": 1,
     "medicine_price": 30,
-    "medicine_image_path": "https://www.drugs.com/medicine_image_paths/pills/fio/A30.jpg",
+    "medicine_image_path": "https://graph.org/file/2d4da81ce43270982abf8.png",
     "use": "Aspirin is a salicylate drug used to reduce pain, fever or inflammation. It is commonly used to treat headaches, muscle aches, arthritis, toothaches, and menstrual cramps.",
     "quantity": "10 strips",
     "type": "tablet",
@@ -11,7 +11,7 @@ final medicines_dummy_list = {
   "Ibuprofen": {
     "medicine_id": 2,
     "medicine_price": 45,
-    "medicine_image_path": "https://www.drugs.com/medicine_image_paths/pills/fio/ADVIL_CAPLET.jpg",
+    "medicine_image_path": "https://graph.org/file/08b0dbced515a3b75a23b.png",
     "use": "Ibuprofen is a nonsteroidal anti-inflammatory drug (NSAID) used to relieve pain, fever, and inflammation. It is commonly used to treat headaches, menstrual cramps, arthritis, toothaches, and back pain.",
     "quantity": "15 strips",
     "type": "tablet",
@@ -20,7 +20,7 @@ final medicines_dummy_list = {
   "Acetaminophen": {
     "medicine_id": 3,
     "medicine_price": 25,
-    "medicine_image_path": "https://www.drugs.com/medicine_image_paths/pills/fio/TYLENOL_500_CAPLET.jpg",
+    "medicine_image_path": "https://graph.org/file/7beac230ddbbdbdfc76c5.png",
     "use": "Acetaminophen is a pain reliever and fever reducer. It is commonly used to treat mild to moderate pain, such as headaches, muscle aches, menstrual cramps, and toothaches.",
     "quantity": "12 strips",
     "type": "tablet",
@@ -29,7 +29,7 @@ final medicines_dummy_list = {
   "Amoxicillin": {
     "medicine_id": 4,
     "medicine_price": 50,
-    "medicine_image_path": "https://www.drugs.com/medicine_image_paths/pills/fio/AMOXIL_250_GLAXO.jpg",
+    "medicine_image_path": "https://graph.org/file/49cd0156f4d1b322dd7fd.png",
     "use": "Amoxicillin is an antibiotic used to treat a variety of bacterial infections, including pneumonia, bronchitis, ear infections, and urinary tract infections.",
     "quantity": "20 capsules",
     "type": "capsule",
@@ -38,7 +38,7 @@ final medicines_dummy_list = {
   "Levofloxacin": {
     "medicine_id": 5,
     "medicine_price": 70,
-    "medicine_image_path": "https://www.drugs.com/medicine_image_paths/pills/fio/LEVAQUIN_500.jpg",
+    "medicine_image_path": "https://graph.org/file/5c4a4c659a0184eeb3754.png",
     "use": "Levofloxacin is an antibiotic used to treat a variety of bacterial infections, including pneumonia, bronchitis, sinusitis, and urinary tract infections.",
     "quantity": "10 tablets",
     "type": "tablet",
@@ -47,7 +47,7 @@ final medicines_dummy_list = {
   "Omeprazole": {
     "medicine_id": 6,
     "medicine_price": 40,
-    "medicine_image_path": "https://www.drugs.com/medicine_image_paths/pills/fio/LOSEC.jpg",
+    "medicine_image_path": "https://graph.org/file/b2ce31961ea0b3dbaad2d.png",
     "use": "Omeprazole is a proton pump inhibitor used to reduce stomach acid production. It is commonly used to treat gastroesophageal reflux disease (GERD), peptic ulcer disease, and Zollinger-Ellison syndrome.",
     "quantity": "14 capsules",
     "type": "capsule",
